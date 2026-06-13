ddd044b77b781e3b3f889b0ee1a2a0b8cefb0418
dkaliakin <dskalyakin@gmail.com>
2026-06-13 11:23:37 -0400

    Added examples of output of rdm_t, rdm_t_lambda, and ci-vec global RDM assembly runs

---
+++ Examples/test_rdm_t/jobs_EWF/step_002/jobs_fragments_production/frag_dump_002.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=ewf_dump_002
+#SBATCH --output=/mnt/beegfs/merzk/kaliakd/EWF_Geom_Opt_Development/2_test_multi_solver/test_rdm_t/jobs_EWF/step_002/jobs_fragments_production/frag_dump_002.out
+#SBATCH --error=/mnt/beegfs/merzk/kaliakd/EWF_Geom_Opt_Development/2_test_multi_solver/test_rdm_t/jobs_EWF/step_002/jobs_fragments_production/frag_dump_002.err
+#SBATCH --partition=merzk
+#SBATCH --ntasks=2
+#SBATCH --mem=10G
+
+set -u
+STATUS_FILE=/mnt/beegfs/merzk/kaliakd/EWF_Geom_Opt_Development/2_test_multi_solver/test_rdm_t/jobs_EWF/step_002/jobs_fragments_production/frag_dump_002.status
+on_exit() {
+    rc=$?
+    if [ "$rc" -eq 0 ]; then
+        echo "DONE" > "$STATUS_FILE"
+    else
+        echo "FAILED $rc" > "$STATUS_FILE"
+    fi
+}
+trap on_exit EXIT
+echo "RUNNING ${SLURM_JOB_ID:-?} $(date -u +%FT%TZ)" > "$STATUS_FILE"
+cd "/mnt/beegfs/merzk/kaliakd/EWF_Geom_Opt_Development/2_test_multi_solver/test_rdm_t"
+export OMP_NUM_THREADS=${SLURM_NTASKS:-1}
+export MKL_NUM_THREADS=${SLURM_NTASKS:-1}
+python /mnt/beegfs/merzk/kaliakd/EWF_Geom_Opt_Development/2_test_multi_solver/test_rdm_t/EWF-CI_Geom_Opt_HPC.py --config /mnt/beegfs/merzk/kaliakd/EWF_Geom_Opt_Development/2_test_multi_solver/test_rdm_t/jobs_EWF/step_002/config.yaml --mode dump --frag-idx 2
